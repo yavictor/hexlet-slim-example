0d7e812fb81bab0e900e7a0038672b56796843e9
YV <yavictor37@yandex.ru>
2020-01-08 12:54:55 +0300

    Add filter form

--- public/index.php
+++ public/index.php
@@ -4,11 +4,45 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
+
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
 
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
 });
+
+$app->get('/users', function ($request, $response) use ($users) {
+    $term = $request->getQueryParam('term');
+    $usersTerm = array_filter($users, function($user) use ($term) {
+        return strpos($user, $term) > 0;
+    });
+    $params = ['users' => $usersTerm];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    //$str = '<script>alert('attack!');</script>';
+
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря http://php.net/manual/ru/closure.bindto.php
+    return $this->get('renderer')->render($response, 'users/showID.phtml', $params);
+});
+
 $app->run();
